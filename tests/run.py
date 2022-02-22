79597488d1d62e8f3ac79bf2483ca6d840fa7310
Shivasai Krishna Thota <sthota@ansa-usa.com>
2022-02-22 10:30:34 +0530

    unittest main method update

--- tests/run.py
+++ tests/run.py
@@ -4,7 +4,7 @@ import unittest
 
 from .unit_test import user_input_test
 
-def main(*args):
+def main():
     suite = unittest.TestLoader().loadTestsFromModule(user_input_test)
     unittest.TextTestRunner(verbosity=2,buffer=True).run(suite)
     return 0
