b70a8fb0c7207ce302e0136f170cc7ee240bd9a6
Anuragreddy-Naredla <anuragreddy1241@gmail.com>
2022-03-01 10:20:00 +0530

    excel_bom_class

--- src/generate_reports/excel_generator.py
+++ src/generate_reports/excel_generator.py
@@ -13,3 +13,19 @@ Developer   : Naresh Medipalli and Shiva Sai
 Date        : Dec 31, 2021
 
 """
+import os
+
+class ExcelBomGeneration():
+    def __init__(self, metadb_3d_input):
+        """
+        __init__ _summary_
+
+        _extended_summary_
+
+        Args:
+            metadb_3d_input (_type_): _description_
+        """
+        self.metadb_3d_input = metadb_3d_input
+    def excel_bom_generation(self, ):
+        critical_section_data = self.metadb_3d_input.critical_sections
+        print(critical_section_data)
