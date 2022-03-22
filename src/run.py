48e110e6b9af1645fa71b0b9f90b2b0ed30160ac
Shivasai Krishna Thota <sthota@ansa-usa.com>
2022-03-22 15:19:44 +0530

    latest updates

--- src/run.py
+++ src/run.py
@@ -51,7 +51,6 @@ def main(*args):
     # Joining the config directory path and log directory path
     general_input_info = GeneralVarInfo()
     log_file = os.path.join(app_config_dir,"res",general_input_info.get_log_directory().replace("/","",1)).replace("\\",os.sep)
-    general_input_info.binout_directory = os.path.join(app_config_dir,"res",general_input_info.binout_directory.replace("/","",1)).replace("\\",os.sep)
     logger = SideCrashLogger(log_file)
 
     # Getting the meta2dinfo and meta3d info
@@ -67,6 +66,7 @@ def main(*args):
     # # Joining the config directory path and d3hsp file path for d3hsp file
     # d3hsp_file_path = os.path.join(app_config_dir,"res",general_input_info.d3hsp_file.replace("/","",1)).replace("\\",os.sep)
     # Getting the spotweld cluster for d3hsp file
+    general_input_info.binout_directory = os.path.join(app_config_dir,"res",general_input_info.binout_directory.replace("/","",1)).replace("\\",os.sep)
     metadb_3d_input_info.get_spotweld_clusters(user_input.d3hsp_file_path)
 
     #reading the results
