3883545b0a0cfff9078b8b7233b03fb6fa3a7664
Shivasai Krishna Thota <sthota@ansa-usa.com>
2022-04-08 12:16:51 +0530

    latest updates

--- src/generate_reports/report_slides/enclosure_performance_rear_door_panel_deformation_slide.py
+++ src/generate_reports/report_slides/enclosure_performance_rear_door_panel_deformation_slide.py
@@ -173,9 +173,20 @@ class EnclosurePerformanceRearDoorPanelDeformationSlide():
                             for plot in door_skin_intrusion_window_plots:
                                 plot.activate()
                                 initial_curve_re  ="*0MS"
-                                final_curve_re = "*150MS"
                                 curves = plot.get_curves('all')
                                 deformation_line_list = []
+                                if self.general_input.survival_space_final_time not in ["null","none",""]:
+                                    final_time = str(round(float(self.general_input.survival_space_final_time)))
+                                    final_time_curve_name = "*{}MS".format(final_time)
+                                    final_curves = plot.get_curves('byname', name = final_time_curve_name)
+                                    if final_curves:
+                                        final_curves[0].show()
+                                    else:
+                                        self.logger.info("ERROR : Door panel intrusion window does not contain Rear '{}' curve from META 2D variable {}. Please update.".format(final_time_curve_name,GeneralVarInfo.survival_space_final_time_key))
+                                        self.logger.info("")
+                                else:
+                                    self.logger.info("ERROR : META 2D variable '{}' is not available or invalid. Please update.".format(GeneralVarInfo.survival_space_final_time_key))
+                                    self.logger.info("")
                                 for each_curve in curves:
                                     ms = each_curve.name.rsplit("_",1)[1]
                                     if 'MS' in ms:
@@ -184,13 +195,18 @@ class EnclosurePerformanceRearDoorPanelDeformationSlide():
                                 if self.general_input.peak_time_display_value not in ["null","none",""]:
                                     peak_time = str(closest(deformation_line_list, round(float(self.general_input.peak_time_display_value))))
                                     peak_curve_re = "*{}MS".format(peak_time)
+                                    peak_curves = plot.get_curves('byname', name = peak_curve_re)
+                                    if peak_curves:
+                                        peak_curves[0].show()
+                                        peak_curves[0].set_line_style(line_style = 5)
+                                    else:
+                                        self.logger.info("ERROR : Door panel intrusion window does not contain Rear '{}' curve from META 2D variable {}. Please update.".format(peak_curve_re,GeneralVarInfo.peak_time_display_key))
+                                        self.logger.info("")
                                 else:
                                     self.logger.info("ERROR : META 2D variable '{}' is not available or invalid. Please update.".format(GeneralVarInfo.peak_time_display_key))
                                     self.logger.info("")
-                                for name in [initial_curve_re,final_curve_re,peak_curve_re]:
-                                    curve = plot.get_curves('byname', name = name)[0]
-                                    curve.show()
-                                curve.set_line_style(line_style = 5)
+                                initial_curve = plot.get_curves('byname', name = initial_curve_re)[0]
+                                initial_curve.show()
                                 utils.MetaCommand('xyplot axisoptions yaxis active "{}" {} 0'.format(door_panel_intrusion_window_name,plot.id))
                                 utils.MetaCommand('xyplot axisoptions ylabel font "{}" {} "Arial,10,-1,5,75,0,0,0,0,0"'.format(door_panel_intrusion_window_name,plot.id))
                                 utils.MetaCommand('xyplot axisoptions labels yfont "{}" {} "Arial,10,-1,5,75,0,0,0,0,0"'.format(door_panel_intrusion_window_name,plot.id))
